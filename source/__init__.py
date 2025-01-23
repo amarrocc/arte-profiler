934613c0c1a475e1841a123e8450db7641b186b8
Alessandra Marrocchesi <marrocchesialessandra@gmail.com>
2025-01-23 17:58:12 +0100

    minor

--- source/__init__.py
+++ source/__init__.py
@@ -1,3 +1,4 @@
 from .profiling import ColorProfileBuilder
+from .profiling_utils import run_command, get_argyll_bin_path
 
-__all__ = ["ColorProfileBuilder"]
+__all__ = ["ColorProfileBuilder", "run_command", "get_argyll_bin_path"]
